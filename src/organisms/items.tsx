d8696a631bb2b43fbc116884aa62be270e66da01
Rômulo Evangelista Ferreira <romuloevangelista2011@gmail.com>
2022-03-28 17:14:52 -0300

    fix: item hashes

--- src/organisms/items.tsx
+++ src/organisms/items.tsx
@@ -1,52 +1,60 @@
-import React from 'react'
-import {
-  Title
-} from '../atoms'
-import { Row } from 'react-bootstrap'
-import SpeedItem from '../molecules/speedItem'
-import IncreaseSpeedImg from '../assets/increaseSpeedPotion.png'
-import DecreaseSpeedImg from '../assets/decreaseSpeedPotion.png'
-
+import React from "react";
+import { Title } from "../atoms";
+import SpeedItem from "../molecules/speedItem";
+import IncreaseSpeedImg from "../assets/increaseSpeedPotion.png";
+import DecreaseSpeedImg from "../assets/decreaseSpeedPotion.png";
 
 const Items = (params: {
-  actor?: {
-    id: string;
-    common_type: number;
-    owner: string;
-    speed: number;
-} | null | undefined,
-  setActor: React.Dispatch<React.SetStateAction<{
-    id: string;
-    common_type: number;
-    owner: string;
-    speed: number;
-} | null | undefined>>
+  actor?:
+    | {
+        id: string;
+        common_type: number;
+        owner: string;
+        speed: number;
+      }
+    | null
+    | undefined;
+  setActor: React.Dispatch<
+    React.SetStateAction<
+      | {
+          id: string;
+          common_type: number;
+          owner: string;
+          speed: number;
+        }
+      | null
+      | undefined
+    >
+  >;
 }) => {
-
   if (!params.actor) {
-    return <Title>Load actor to see the available items</Title>
+    return <Title>Load actor to see the available items</Title>;
   }
 
-  return <div style={{
-    display: 'flex',
-    width: '70vh',
-    justifyContent: 'space-between'
-  }}>
-    <SpeedItem 
-      actor={params.actor}
-      setActor={params.setActor}
-      itemAsset={IncreaseSpeedImg}
-      itemId='0xda3a1102e7951d1c09edd50605ea5321271b8809d9f523762b8f534d7fb9e98c'
-      itemName='Increase speed potion'
-    />
-    <SpeedItem 
-      actor={params.actor}
-      setActor={params.setActor}
-      itemAsset={DecreaseSpeedImg}
-      itemId='0xa57587e2a41d41d88f8157b7b679b11ea33013660e140c880711085585fa51fc'
-      itemName='Decrease speed potion'
-    />
-  </div>
-}
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "70vh",
+        justifyContent: "space-between",
+      }}
+    >
+      <SpeedItem
+        actor={params.actor}
+        setActor={params.setActor}
+        itemAsset={IncreaseSpeedImg}
+        itemId="0x2372c9bb4e2008aa42b7c67533af805aa7477eaac133e16f617ce9e1af888f34"
+        itemName="Increase speed potion"
+      />
+      <SpeedItem
+        actor={params.actor}
+        setActor={params.setActor}
+        itemAsset={DecreaseSpeedImg}
+        itemId="0xc71943886a686e3ae37929e0c4ab9074675eef04c1fa69f363af432a89756ab6"
+        itemName="Decrease speed potion"
+      />
+    </div>
+  );
+};
 
-export default Items
+export default Items;
